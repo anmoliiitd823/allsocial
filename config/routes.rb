2fea77b89b873219e89f00c78e7f3d54790e136c
anmoliiitd823 <32006941+anmoliiitd823@users.noreply.github.com>
2017-10-09 20:33:46 +0530

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
+
   	
   post 'friendships/create'
   get 'friendships/create'
@@ -9,20 +9,18 @@ Rails.application.routes.draw do
 	post 'likes/toggle_comment_like/' => 'likes#toggle_comment_like'
 	post 'likes/toggle_post_like/' => 'likes#toggle_post_like'
 
-=======
->>>>>>> 9c85a46a125b76c917caa1a48018fc6aad094960
+
   get 'comments/create'
 
   get 'comments/destroy'
 
   devise_for :users
-<<<<<<< HEAD
+
   get 'users/filter_by_email' => 'users#filter_by_email'
   get 'users/:id' => 'users#show', as: :user_show
   get 'user/:id' => 'users#show'
   patch 'user/:id' => 'users#update', as: :user
-=======
->>>>>>> 9c85a46a125b76c917caa1a48018fc6aad094960
+
   resources :posts, only: [:index, :create, :destroy]
   resources :comments, only: [:create, :destroy]
 
